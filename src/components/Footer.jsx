f24902448711423bfb177d8be6a4931c424904de
mpicodW <martin.picod@getweloop.io>
2020-02-21 11:40:26 +0100

    try something

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'react-emotion';
-import { Link } from 'gatsby';
-import { Flex } from 'grid-emotion';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "react-emotion";
+import { Link } from "gatsby";
+import { Flex } from "grid-emotion";
 
 const Wrapper = styled.footer`
   background: ${props => props.theme.colors.greyDark};
@@ -30,7 +30,7 @@ const StyledLink = styled(Link)`
   transform: translateX(0);
   transition: all 200ms ease-out;
   &:before {
-    content: '←';
+    content: "←";
     padding-right: 8px;
   }
   &:hover {
@@ -44,7 +44,142 @@ const Footer = ({ isCase }) => {
   return (
     <Wrapper>
       <Inner justifyContent="space-between" p={4}>
-        {isCase ? (
+        <footer class="container py-5">
+          <div class="row">
+            <div class="col-12 col-md">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="d-block mb-2"
+                role="img"
+                viewBox="0 0 24 24"
+                focusable="false"
+              >
+                <title>Product</title>
+                <circle cx="12" cy="12" r="10"></circle>
+                <path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"></path>
+              </svg>
+              <small class="d-block mb-3 text-muted">© 2017-2019</small>
+            </div>
+            <div class="col-6 col-md">
+              <h5>Features</h5>
+              <ul class="list-unstyled text-small">
+                <li>
+                  <a class="text-muted" href="#">
+                    Cool stuff
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Random feature
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Team feature
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Stuff for developers
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Another one
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Last time
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-6 col-md">
+              <h5>Resources</h5>
+              <ul class="list-unstyled text-small">
+                <li>
+                  <a class="text-muted" href="#">
+                    Resource
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Resource name
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Another resource
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Final resource
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-6 col-md">
+              <h5>Resources</h5>
+              <ul class="list-unstyled text-small">
+                <li>
+                  <a class="text-muted" href="#">
+                    Business
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Education
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Government
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Gaming
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-6 col-md">
+              <h5>About</h5>
+              <ul class="list-unstyled text-small">
+                <li>
+                  <a class="text-muted" href="#">
+                    Team
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Locations
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Privacy
+                  </a>
+                </li>
+                <li>
+                  <a class="text-muted" href="#">
+                    Terms
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </footer>
+        {/* {isCase ? (
           <React.Fragment>
             <StyledLink to="/">Return to home</StyledLink>
             <div>Contact Us</div>
@@ -59,7 +194,7 @@ const Footer = ({ isCase }) => {
             </div>
             <div>Snapchat &mdash; Musicaly &mdash; Twitter</div>
           </React.Fragment>
-        )}
+        )} */}
       </Inner>
     </Wrapper>
   );
@@ -68,9 +203,9 @@ const Footer = ({ isCase }) => {
 export default Footer;
 
 Footer.propTypes = {
-  isCase: PropTypes.bool,
+  isCase: PropTypes.bool
 };
 
 Footer.defaultProps = {
-  isCase: false,
+  isCase: false
 };
--- src/templates/case.jsx
+++ src/templates/case.jsx
@@ -1,95 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
-import Helmet from 'react-helmet';
-import styled from 'react-emotion';
-import Img from 'gatsby-image';
-import { Box } from 'grid-emotion';
-import Layout from '../components/Layout';
-import Footer from '../components/Footer';
-import SEO from '../components/SEO';
-import config from '../../config/website';
-
-const Hero = styled.section`
-  width: 100%;
-  height: 100vh;
-  position: relative;
-  overflow: hidden;
-  .gatsby-image-outer-wrapper {
-    position: static !important;
-    > div {
-      position: static !important;
-    }
-  }
-`;
-
-const Wrapper = styled(Box)`
-  max-width: ${props => props.theme.maxWidth};
-`;
-
-const TitleWrapper = styled(Box)`
-  width: 100%;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: ${props => props.theme.colors.bg};
-`;
-
-const Title = styled.h1`
-  color: ${props => props.theme.colors.text};
-  max-width: ${props => props.theme.maxWidthText};
-  text-align: center;
-  margin: 0 auto;
-  padding: 0 32px;
-`;
-
-const SubTitle = styled.h3`
-  color: ${props => props.theme.colors.text};
-  max-width: ${props => props.theme.maxWidthText};
-  margin: 0 auto;
-  text-align: center;
-`;
-
-const Content = styled.main`
-  margin-top: 9rem;
-  margin-bottom: 9rem;
-  p {
-    text-align: justify;
-  }
-  p,
-  ul,
-  ol,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  pre,
-  blockquote {
-    max-width: ${props => props.theme.maxWidthText};
-    margin-left: auto;
-    margin-right: auto;
-  }
-  li {
-    margin-left: 1.45rem;
-  }
-  .block-img {
-    max-width: 100%;
-    margin-top: 6rem;
-    margin-bottom: 6rem;
-    text-align: center;
-  }
-  @media (max-width: ${props => props.theme.breakpoint.m}) {
-    margin-top: 6rem;
-    margin-bottom: 6rem;
-    .block-img {
-      margin-top: 3rem;
-      margin-bottom: 3rem;
-    }
-  }
-`;
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Helmet from "react-helmet";
+import Img from "gatsby-image";
+import Layout from "../components/Layout";
+import Footer from "../components/Footer";
+import SEO from "../components/SEO";
+import config from "../../config/website";
 
 const CaseTemplate = ({ data: { prismicCaseStudy: caseNode } }) => {
   const { data } = caseNode;
@@ -116,8 +33,8 @@ export default CaseTemplate;
 
 CaseTemplate.propTypes = {
   data: PropTypes.shape({
-    prismicCaseStudy: PropTypes.object.isRequired,
-  }).isRequired,
+    prismicCaseStudy: PropTypes.object.isRequired
+  }).isRequired
 };
 
 export const pageQuery = graphql`
@@ -130,7 +47,11 @@ export const pageQuery = graphql`
         header_image {
           localFile {
             childImageSharp {
-              fluid(maxWidth: 1920, quality: 90, traceSVG: { color: "#021212" }) {
+              fluid(
+                maxWidth: 1920
+                quality: 90
+                traceSVG: { color: "#021212" }
+              ) {
                 ...GatsbyImageSharpFluid_withWebp_tracedSVG
               }
               resize(width: 800) {
