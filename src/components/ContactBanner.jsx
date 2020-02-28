a525f3de6210d4b8ed428a95129b1d46ff6cd641
mpicodW <martin.picod@getweloop.io>
2020-02-28 13:07:42 +0100

    sdsds

--- src/components/ContactBanner.jsx
+++ src/components/ContactBanner.jsx
@@ -28,7 +28,7 @@ export default class ContactBanner extends Component {
             <form
               className="mt-8 sm:flex flex-col"
               method="post"
-              action="https://gatsby-starter-portfolio-bella.now.sh/"
+              action="https://gatsby-starter-portfolio-bella.now.sh"
               // onSubmit=handle()
             >
               <input
