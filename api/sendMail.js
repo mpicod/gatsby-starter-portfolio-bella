6faeb9d9fc10fbc1566e7838dc5a34416714549c
mpicodW <martin.picod@getweloop.io>
2020-02-29 19:45:57 +0100

    push mail

--- api/sendMail.js
+++ api/sendMail.js
@@ -72,7 +72,9 @@ module.exports = (req, res) => {
     from: req.body.email || "mktg.zenops360@gmail.com",
     to: "mktg.zenops360@gmail.com",
     subject: "ZenOps 360 Contact ",
-    text: req.body.message + req.body.email || "[No message]" + req.body.email
+    text:
+      req.body.message + "   " + req.body.email ||
+      "[No message]" + "   " + req.body.email
   };
   console.log(req.body.email, req.body.message, "DATAMAGGLE)");
   transporter.sendMail(mailOptions, function(error, info) {
