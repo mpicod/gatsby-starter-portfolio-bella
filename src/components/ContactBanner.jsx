91e253503cf3c2d4427b7d4ceb7aea7e5b8b147e
mpicodW <martin.picod@getweloop.io>
2020-02-29 18:56:53 +0100

    add contactform

--- src/components/ContactBanner.jsx
+++ src/components/ContactBanner.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Map from "../images/map.png";
+import ContactForm from "./ContactForm";
 
 export default class ContactBanner extends Component {
   render() {
@@ -12,47 +13,7 @@ export default class ContactBanner extends Component {
             alt="Support team"
           />
         </div>
-        <div className="relative max-w-screen-xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
-          <div className="md:ml-auto md:w-1/2 md:pl-10">
-            <div className="text-base leading-6 font-semibold uppercase tracking-wider text-gray-300">
-              Commencez maintenant
-            </div>
-            <h2 className="mt-2 text-white text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl sm:leading-10">
-              Contactez-nous !
-            </h2>
-            <p className="mt-3 text-lg leading-7 text-gray-300">
-              ZenOps360 et ses experts se tiennent à votre disposition pour
-              répondre à vos demandes ! N'hésitez pas à nous laisser un message
-              et nous reviendrons vers vous rapidement.
-            </p>
-            <form
-              className="mt-8 sm:flex flex-col"
-              method="POST"
-              action="https://gatsby-starter-portfolio-bella.now.sh/api/sendMail"
-              // onSubmit=handle()
-            >
-              <input
-                aria-label="Email address"
-                type="email"
-                required
-                className="mt-3 appearance-none w-full px-5 py-3 border border-gray-300 text-base leading-6 rounded-md text-gray-900 bg-white placeholder-gray-500 focus:outline-none focus:shadow-outline focus:border-blue-300 transition duration-150 ease-in-out sm:max-w-xs"
-                placeholder="Enter your email"
-              />
-              <input
-                aria-label="Message"
-                type="textarea"
-                required
-                className="mt-3  appearance-none w-full px-5 py-3 border border-gray-300 text-base leading-6 rounded-md text-gray-900 bg-white placeholder-gray-500 focus:outline-none focus:shadow-outline focus:border-blue-300 transition duration-150 ease-in-out sm:max-w-xs"
-                placeholder="Enter your message"
-              />
-              <div className="my-3 mt-3 rounded-md shadow sm:flex-shrink-0">
-                <button className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-primary-600 hover:bg-primary-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out sm:max-w-xs">
-                  Envoyer le message
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+        <ContactForm />
       </div>
     );
   }
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -48,7 +48,7 @@ export default () => {
     // console.log("ENV API", API_URL);
     axios({
       method: "POST",
-      url: `${process.env.API_URL}/api/sendMail`,
+      url: `https://gatsby-starter-portfolio-bella.now.sh/api/sendMail`,
       data: inputs,
       headers: {}
     })
@@ -66,9 +66,63 @@ export default () => {
   };
   return (
     <main>
-      <h1>React and Formspree</h1>
+      <div className="relative max-w-screen-xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+        <div className="md:ml-auto md:w-1/2 md:pl-10">
+          <div className="text-base leading-6 font-semibold uppercase tracking-wider text-gray-300">
+            Commencez maintenant
+          </div>
+          <h2 className="mt-2 text-white text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl sm:leading-10">
+            Contactez-nous !
+          </h2>
+          <p className="mt-3 text-lg leading-7 text-gray-300">
+            ZenOps360 et ses experts se tiennent à votre disposition pour
+            répondre à vos demandes ! N'hésitez pas à nous laisser un message et
+            nous reviendrons vers vous rapidement.
+          </p>
+          <form
+            className="mt-8 sm:flex flex-col"
+            method="POST"
+            action="https://gatsby-starter-portfolio-bella.now.sh/api/sendMail"
+            onSubmit={handleOnSubmit}
+          >
+            <input
+              aria-label="Email address"
+              type="email"
+              className="mt-3 appearance-none w-full px-5 py-3 border border-gray-300 text-base leading-6 rounded-md text-gray-900 bg-white placeholder-gray-500 focus:outline-none focus:shadow-outline focus:border-blue-300 transition duration-150 ease-in-out sm:max-w-xs"
+              placeholder="Enter your email"
+              onChange={handleOnChange}
+              required
+              value={inputs.email}
+            />
+            <input
+              aria-label="Message"
+              type="textarea"
+              required
+              className="mt-3  appearance-none w-full px-5 py-3 border border-gray-300 text-base leading-6 rounded-md text-gray-900 bg-white placeholder-gray-500 focus:outline-none focus:shadow-outline focus:border-blue-300 transition duration-150 ease-in-out sm:max-w-xs"
+              placeholder="Enter your message"
+              onChange={handleOnChange}
+              value={inputs.message}
+            />
+            <div className="my-3 mt-3 rounded-md shadow sm:flex-shrink-0">
+              <button className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-primary-600 hover:bg-primary-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out sm:max-w-xs">
+                {!status.submitting
+                  ? !status.submitted
+                    ? "Envoyer le message!"
+                    : "Envoyé!"
+                  : "Envoi en cours..."}
+              </button>
+              {status.info.error && (
+                <div className="error">Error: {status.info.message}</div>
+              )}
+              {!status.info.error && status.info.message && (
+                <p>{status.info.message}</p>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
       <hr />
-      <form onSubmit={handleOnSubmit}>
+      {/* <form onSubmit={handleOnSubmit}>
         <label htmlFor="email">Email</label>
         <input
           id="email"
@@ -99,7 +153,7 @@ export default () => {
       )}
       {!status.info.error && status.info.message && (
         <p>{status.info.message}</p>
-      )}
+      )} */}
     </main>
   );
 };
