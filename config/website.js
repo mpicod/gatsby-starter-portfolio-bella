8326b7888dee2ed6861813fc9b462ebe0c74d6aa
mpicodW <martin.picod@getweloop.io>
2020-03-19 14:32:34 +0100

    Update website.js

--- config/website.js
+++ config/website.js
@@ -1,20 +1,20 @@
 module.exports = {
   pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: "ZenOps - Home", // Navigation and Site Title
+  siteTitle: "ZenOps", // Navigation and Site Title
   siteTitleAlt: "ZenOps", // Alternative Site title for SEO
-  siteUrl: "https://portfolio-bella.netlify.com", // Domain of your site. No trailing slash!
+  siteUrl: "zenops.fr", // Domain of your site. No trailing slash!
   siteLanguage: "fr", // Language Tag on <html> element
-  siteLogo: "/logos/logo-1024.png", // Used for SEO and manifest
+  siteLogo: "/logos/ms-icon-310x310.png", // Used for SEO and manifest
   siteDescription: "ZenOps company website",
   author: "mpicod", // Author for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: "@bella", // Twitter Username
-  ogSiteName: "bella", // Facebook Site Name
+  userTwitter: "@mpicod", // Twitter Username
+  ogSiteName: "", // Facebook Site Name
   ogLanguage: "en_US", // Facebook Language
 
   // Manifest and Progress color
-  themeColor: "#b710a1",
-  backgroundColor: "#350d27"
+  themeColor: "#3AB97A",
+  backgroundColor: "#1A202C"
 };
