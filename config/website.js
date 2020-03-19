0b217ff864dc741bf78fe04d1473bf66c066c042
mpicodW <martin.picod@getweloop.io>
2020-03-19 14:38:50 +0100

    Update website.js

--- config/website.js
+++ config/website.js
@@ -3,7 +3,7 @@ module.exports = {
 
   siteTitle: "ZenOps", // Navigation and Site Title
   siteTitleAlt: "ZenOps", // Alternative Site title for SEO
-  siteUrl: "zenops.fr", // Domain of your site. No trailing slash!
+  siteUrl: "https//:zenops.fr", // Domain of your site. No trailing slash!
   siteLanguage: "fr", // Language Tag on <html> element
   siteLogo: "/logos/ms-icon-310x310.png", // Used for SEO and manifest
   siteDescription: "ZenOps company website",
@@ -11,7 +11,7 @@ module.exports = {
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: "@mpicod", // Twitter Username
-  ogSiteName: "", // Facebook Site Name
+  ogSiteName: "zenops", // Facebook Site Name
   ogLanguage: "en_US", // Facebook Language
 
   // Manifest and Progress color
